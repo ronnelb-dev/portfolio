f60de22747eb89dbc60a55c56d466277a1a58dc5
ronnelb <barasharironnel29@gmail.com>
2023-05-18 02:19:53 +0800

    update

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slide } from "react-awesome-reveal";
+import myResume from '../Files/ronnel_resume.pdf';
 
 class ContactSection extends React.Component {
   render() {
@@ -22,6 +23,14 @@ class ContactSection extends React.Component {
                                     Say Hello
                                 </span>
                             </a>
+                        </div>
+
+                        <div class="flex justify-center mt-10">
+                        <a href={myResume} download="RonnelResume.pdf" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                            My Resume
+                        </span>
+                        </a>
                         </div>
                     </div>
                 </Slide>

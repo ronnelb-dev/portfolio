2194be9da550889432db70433d9a4ea7f40cb6fd
ronnelb <barasharironnel29@gmail.com>
2026-04-16 10:39:17 +0800

    update project data

--- src/Components/FooterSection.js
+++ src/Components/FooterSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FaGithub, FaLinkedin, FaFacebook, FaEnvelope, FaChevronUp } from 'react-icons/fa';
 
 const NAV_LINKS = [
